323237f1313061a94058039419ea7abac5124b39
JedikOps <jedikops@gmail.com>
2026-06-01 23:37:37 +0200

    invoices table pagination add invoices

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,16 +2,20 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams.toString());
+    params.set("page", "1"); // Reset to first page on new search
+    
     if (term) {
       params.set("query", term);
     } else {
@@ -19,7 +23,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
