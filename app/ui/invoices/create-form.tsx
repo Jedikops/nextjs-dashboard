37e6aecd23f6511a16759dd685da02a9ae50d524
JedikOps <jedikops@gmail.com>
2026-06-03 00:50:07 +0200

    auto-set on validation error (buggy)

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,9 +10,21 @@ import {
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
 import { createInvoice, State } from "@/app/lib/actions";
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const [formInitData, setFormData] = useState<{
+    customerId: string;
+    amount: number | string;
+    status: string;
+  }>({ customerId: "", amount: "", status: "pending" });
+
+  const onRadioChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFormData((prev) => ({
+      ...prev,
+      status: e.target.value,
+    }));
+
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
 
@@ -29,8 +41,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby="customer-error"
+              {...(formInitData.customerId !== undefined ||
+              formInitData.customerId
+                ? { value: formInitData.customerId }
+                : { defaultValue: "" })}
+              onChange={(e) =>
+                setFormData((prev) => ({
+                  ...prev,
+                  customerId: e.target.value,
+                }))
+              }
             >
               <option value="" disabled>
                 Select a customer
@@ -71,6 +91,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
+                value={formInitData.amount}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    amount: Number(e.target.value) || "",
+                  }))
+                }
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -102,6 +129,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                   aria-describedby="status-error"
+                  checked={formInitData.status === "pending"}
+                  onChange={onRadioChange}
                 />
                 <label
                   htmlFor="pending"
@@ -118,6 +147,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                   aria-describedby="status-error"
+                  checked={formInitData.status === "paid"}
+                  onChange={onRadioChange}
                 />
                 <label
                   htmlFor="paid"
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,7 @@ import {
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
 import { updateInvoice, State } from "@/app/lib/actions";
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,6 +19,7 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
